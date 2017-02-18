87e44653f5fecc9f6240d9345452fae672b0e1b7
FerencOnodi <onodiferenc91@gmail.com>
2017-02-18 12:52:42 +0100

    Task 4 implemented.

---
+++ src/NemLetezoAruKivetel.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ferenc on 2017.02.18..
+ */
+public class NemLetezoAruKivetel extends Exception {
+
+    public NemLetezoAruKivetel(String message) {
+    }
+}
---
+++ src/TulSokLevonasKivetel.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ferenc on 2017.02.18..
+ */
+public class TulSokLevonasKivetel extends Exception {
+
+    public TulSokLevonasKivetel(String message) {
+    }
+}
